8b86e3a4820f693548474f20f8b8503fb38efa01
Jon Durbin <jon.durbin@onna.com>
2022-06-09 07:26:59 -0400

    Fix os image.

--- guillotina_elasticsearch/tests/conftest.py
+++ guillotina_elasticsearch/tests/conftest.py
@@ -1,14 +1,15 @@
 from pytest_docker_fixtures import images
 
 
-image_version = "7.5.1"
+image_version = "2.0.0"
 
 images.configure(
-    "opensearch",
+    "elasticsearch",
     "opensearchproject/opensearch",
     image_version,
     max_wait_s=90,
     env={
+        "xpack.security.enabled": None,
         "plugins.security.disabled": "true",
         "discovery.type": "single-node",
         "http.host": "0.0.0.0",
